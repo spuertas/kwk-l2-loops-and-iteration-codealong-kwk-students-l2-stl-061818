bd486f9bd4c144082b8b7e72443ada418111b66d
Sophia <spuertas@micds.org>
2018-06-21 16:40:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function tailsNeverFails() {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++
+  }
+  console.log(`You got ${tails} tails in a row!`);
+}
+for (let t = 0; t <= 1000000; t++) {
+  tailsNeverFails();
+}
